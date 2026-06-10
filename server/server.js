33f079c7d6ff1126ea401ed73d9e2ac3815cd9b5
beautyDML <beauty.rani@digimantra.com>
2026-06-10 20:44:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./src/config/db.js";
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
